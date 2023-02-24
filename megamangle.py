e7c46a87e9d232aa41cbc04589a96531f9745c7f
John A Lappas Jr <johnlappasjr@icloud.com>
2023-02-24 13:39:26 -0500

    Added support for multprocessing

--- megamangle.py
+++ megamangle.py
@@ -1,27 +1,33 @@
 from PIL import Image
 import os
+from multiprocessing import Pool
 
-# Set the desired size in megapixels
-megapixels = 20
-
-# Set the input and output directories
-input_dir = '/Users/someuser/Desktop/images/'
-output_dir = '/Users/someuser/Desktop/output/'
-
-# Iterate through all files in the input directory
-for filename in os.listdir(input_dir):
-    # Check if the file is a PNG
-    if filename.lower().endswith('.png'):
-        # Open the image
-        img_path = os.path.join(input_dir, filename)
-        with Image.open(img_path) as im:
-            # Calculate the desired size in pixels
+def resize_image(file_path):
+    megapixels = 20
+    input_dir = 'path/to/input/directory'
+    output_dir = 'path/to/output/directory'
+    if file_path.lower().endswith('.png'):
+        print(f"Resizing {file_path}")
+        with Image.open(file_path) as im:
             megapixels_in_pixels = megapixels * 1000000
             ratio = (megapixels_in_pixels / (im.width * im.height)) ** 0.5
             width = int(im.width * ratio)
             height = int(im.height * ratio)
-            # Resize the image
             resized_im = im.resize((width, height))
-            # Save the resized image in the output directory
-            output_path = os.path.join(output_dir, filename)
+            output_path = os.path.join(output_dir, os.path.relpath(file_path, input_dir))
+            os.makedirs(os.path.dirname(output_path), exist_ok=True)
             resized_im.save(output_path)
+            print(f"Saved resized image to {output_path}")
+
+if __name__ == '__main__':
+    input_dir = 'path/to/input/directory'
+    output_dir = 'path/to/output/directory'
+    pool = Pool(processes=6) # Change the number of processes to suit your system
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            pool.apply_async(resize_image, args=(file_path,))
+            print(f"Scheduled {file_path} for resizing")
+    pool.close()
+    pool.join()
+    print("Done") 
